74b8d262dcf3b0b24c45259d5d80dac7b45d091b
suryakiran <skparzival264@gmail.com>
2023-06-01 20:52:05 +0530

    UDP corrected and updated

--- Socket-Programming/UDP/server.c
+++ Socket-Programming/UDP/server.c
@@ -16,7 +16,6 @@ int main()
     //steps involved in defining the serveraddress.
     bzero((char*)&serveraddr,sizeof(serveraddr)); //initializing the server address to zero
     serveraddr.sin_family=AF_INET;//setting the family of the server address to AF_INET
-
     printf("Enter the port number ");
     scanf("%d",&port);
     serveraddr.sin_port=htons(port);//setting the port number of the server address to port , htons is used to convert the port number to network byte order
@@ -26,30 +25,21 @@ int main()
     printf("\nWaiting for the client connection\n");
     bzero((char*)&clientaddr,sizeof(clientaddr));//initializing the client address to zero
     len=sizeof(clientaddr);//storing the length of the client address in len
-
     //receiving message from the client.
     recvfrom(serversocket,message,sizeof(message),0,(struct sockaddr*)&clientaddr,&len);//recvfrom is used to receive the message from the client
     printf("\nConnection received from client.\n");
-    printf("\nThe client has send:\t%s\n",message);
+    printf("\nThe client has send:\n%s\n",message);
     printf("\nSending message to the client.\n");
+    int msg_len=strlen(message);
+    int start = 0, end = msg_len - 1;
+	while (start < end) {
+		char temp = message[start];
+		message[start] = message[end];
+		message[end] = temp;
+		start++;
+		end--;
+	}
     //sending message to the client.
-    sendto(serversocket,"YOUR MESSAGE RECEIVED.",sizeof("YOUR MESSAGERECEIVED."),0,( struct sockaddr*)&clientaddr,sizeof(clientaddr));//sendto is used to send the message to the client
+    sendto(serversocket,message,sizeof(message),0,( struct sockaddr*)&clientaddr,sizeof(clientaddr));//sendto is used to send the message to the client
     close(serversocket);
 }
-
-
-
-/*
-s6d2@user-HP-280-G3-MT:~/Networking-Lab-S6/Socket-Programming/UDP$ gcc server.c -o server
-s6d2@user-HP-280-G3-MT:~/Networking-Lab-S6/Socket-Programming/UDP$ ./server
-Enter the port number 6000
-
-Waiting for the client connection
-
-Connection received from client.
-
-The client has send:    HI I AM CLIENT...
-
-Sending message to the client.
-s6d2@user-HP-280-G3-MT:~/Networking-Lab-S6/Socket-Programming/UDP$ 
-*/
