464043d635317e04b77aea2f3dcecdd76b705dba
suryakiran <skparzival264@gmail.com>
2023-06-03 11:06:02 +0530

    updated tcp client

--- File Transfer Protocol/client.c
+++ File Transfer Protocol/client.c
@@ -3,31 +3,28 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
-#include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-#include <arpa/inet.h>
-
-#define SERV_TCP_PORT 5035
+#include<arpa/inet.h>
+#define TCP_PORT 5035
 #define MAX 60
-int main(int arg,char*argv[])
+int main()
 {
-       int sockfd,n;
-       struct sockaddr_in serv_addr;
-       struct hostent*server;
-       char send[MAX],recvline[MAX],s[MAX],name[MAX];
-       sockfd=socket(AF_INET,SOCK_STREAM,0);
-       serv_addr.sin_family=AF_INET;
-       serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-       serv_addr.sin_port=htons(SERV_TCP_PORT);
-       connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
-       printf("\nEnter the source file name : \n");
-       scanf("%s",send);
-       write(sockfd,send,MAX);
-       while((n=read(sockfd,recvline,MAX))!=0)
-       {
-          printf("%s",recvline);
-       }
-       close(sockfd);
-       return 0;
+   int clientsocket,n;
+   struct sockaddr_in serveraddr;
+   char send[MAX],recvline[MAX],s[MAX],name[MAX];
+   clientsocket=socket(AF_INET,SOCK_STREAM,0);
+   serveraddr.sin_family=AF_INET;
+   serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+   serveraddr.sin_port=htons(TCP_PORT);
+   connect(clientsocket,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+   printf("\nEnter the source file name : \n");
+   scanf("%s",send);
+   write(clientsocket,send,MAX);
+   while((n=read(clientsocket,recvline,MAX))!=0)
+   {
+      printf("%s",recvline);
+   }
+   close(clientsocket);
+   return 0;
 }
--- File Transfer Protocol/server.c
+++ File Transfer Protocol/server.c
@@ -7,37 +7,37 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-#define SERV_TCP_PORT 5035
+#define TCP_PORT 5035
 #define MAX 60
 int i, j, tem;
 char buff[4096], t;
 FILE *f1;
-int main(int afg, char *argv)
+int main()
 {
-       int sockfd, newsockfd, clength;
-       struct sockaddr_in serv_addr,cli_addr;
-       char t[MAX], str[MAX];
-       strcpy(t,"exit");
-       sockfd=socket(AF_INET, SOCK_STREAM,0);
-       serv_addr.sin_family=AF_INET;
-       serv_addr.sin_addr.s_addr=INADDR_ANY;
-       serv_addr.sin_port=htons(SERV_TCP_PORT);
-       printf("\nBinded");
-       bind(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr));
-       printf("\nListening...");
-       listen(sockfd, 5);
-       clength=sizeof(cli_addr);
-       newsockfd=accept(sockfd,(struct sockaddr*) &cli_addr,&clength);
-       close(sockfd);
-       read(newsockfd, &str, MAX);
-       printf("\nClient message\n File Name : %s\n", str);
-       f1=fopen(str, "r");
-       while(fgets(buff, 4096, f1)!=NULL)
-       {
-            write(newsockfd, buff,MAX);
-            printf("\n");
-       }
-       fclose(f1);
-       printf("\nFile Transferred\n");
-       return 0;
+     int serversocket, clientsocket, clength;
+     struct sockaddr_in serveraddr,clientaddr;
+     char t[MAX], str[MAX];
+     strcpy(t,"exit");
+     serversocket=socket(AF_INET, SOCK_STREAM,0);
+     serveraddr.sin_family=AF_INET;
+     serveraddr.sin_addr.s_addr=INADDR_ANY;
+     serveraddr.sin_port=htons(TCP_PORT);
+     printf("\nBinded");
+     bind(serversocket,(struct sockaddr*)&serveraddr, sizeof(serveraddr));
+     printf("\nListening...");
+     listen(serversocket, 5);
+     clength=sizeof(clientaddr);
+     clientsocket=accept(serversocket,(struct sockaddr*) &clientaddr,&clength);
+     close(serversocket);
+     read(clientsocket, &str, MAX);
+     printf("\nClient message\n File Name : %s\n", str);
+     f1=fopen(str, "r");
+     while(fgets(buff, 4096, f1)!=NULL)
+     {
+          write(clientsocket, buff,MAX);
+          printf("\n");
+     }
+     fclose(f1);
+     printf("\nFile Transferred\n");
+     return 0;
 }
